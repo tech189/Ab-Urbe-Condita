7619246f968b6e2946b0620202c131e1f07cbe60
tech189 <dlloyd189@gmail.com>
2021-06-18 12:10:05 +0100

    Update int_to_roman

--- auc.py
+++ auc.py
@@ -29,7 +29,7 @@ def int_to_roman(num):
         1000000, 900000, 500000, 400000, 100000, 90000, 50000, 40000, 10000, 9000, 5000, 4000, 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
 
     _strings = [
-        'M', 'C', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        'M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
 
     result = ""
     decimal = num
@@ -38,7 +38,7 @@ def int_to_roman(num):
         for i in range(len(_values)):
             if decimal >= _values[i]:
                 if _values[i] > 1000:
-                    result += u'\u0304'.join(list(_strings[i])) + u'\u0304'
+                    result += u'\u0305'.join(list(_strings[i])) + u'\u0305'
                 else:
                     result += _strings[i]
                 decimal -= _values[i]
